1146e5cb8d1a6afaded85c8ca56cd411aad7c81e
felcross <feljava32@gmail.com>
2021-08-25 16:38:27 -0300

    metodos save, del, getall feitos

--- src/firebase/db/Cclient.ts
+++ src/firebase/db/Cclient.ts
@@ -4,7 +4,7 @@ import ClientRepo from "../../core/ClientRepo";
 
 export default class Cclient implements ClientRepo {
 
-      converter = {
+      #converter = {
           toFirestore(client:Client){
               return {
                   name:client.name,
@@ -12,7 +12,7 @@ export default class Cclient implements ClientRepo {
               }
           },
 
-        fromFirestore(snapshot:firebase.firestore.QueryDocumentSnapshot, options:firebase.firestore.SnapshotOptions)
+        fromFirestore(snapshot:firebase.firestore.QueryDocumentSnapshot, options:firebase.firestore.SnapshotOptions): Client
         {   const data = snapshot.data(options)
             return new Client(data.name, data.age,snapshot.id)
         }
@@ -20,14 +20,26 @@ export default class Cclient implements ClientRepo {
 
 
     async save(client:Client): Promise<Client>{
-        return null
+       if(client?.id){
+          await this.collection().doc(client.id).set(client)
+          return client
+       } else {
+            const docRef = await this.collection().add(client)
+            const doc = await docRef.get()
+            return doc.data()
+       }
     }
 
     async delete(client:Client): Promise<void>{
-        return null
+        return this.collection().doc(client.id).delete()
     }
 
     async getAll(): Promise<Client[]>{
-        return null
+        const query = await this.collection().get()
+        return query.docs.map(doc => doc.data()) ?? []
     }
+
+    private collection(){
+        return firebase.firestore().collection('client').withConverter(this.#converter)
+    } 
 }
