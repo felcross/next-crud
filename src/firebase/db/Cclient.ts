47f5bde76803b4e8120516ee61f2170e4c2950db
felcross <feljava32@gmail.com>
2021-08-25 16:09:00 -0300

    criando repo de client pra interagir com o firebas

--- src/firebase/db/Cclient.ts
+++ src/firebase/db/Cclient.ts
@@ -1,7 +1,23 @@
 import Client from "../../core/Client";
+import firebase from "../config";
 import ClientRepo from "../../core/ClientRepo";
 
 export default class Cclient implements ClientRepo {
+
+      converter = {
+          toFirestore(client:Client){
+              return {
+                  name:client.name,
+                  age:client.age
+              }
+          },
+
+        fromFirestore(snapshot:firebase.firestore.QueryDocumentSnapshot, options:firebase.firestore.SnapshotOptions)
+        {   const data = snapshot.data(options)
+            return new Client(data.name, data.age,snapshot.id)
+        }
+      }
+
 
     async save(client:Client): Promise<Client>{
         return null
